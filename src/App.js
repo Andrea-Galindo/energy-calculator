94e0a5d4062ec80982816096754f1901676fa697
Andrea <andreagalindo.guerra@gmail.com>
2023-05-30 17:05:13 -0400

    Adds mui library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import EnergyForm from "./components/EnergyForm";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <EnergyForm />
     </div>
   );
 }
