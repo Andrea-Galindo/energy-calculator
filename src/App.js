15d5796e2470bce4b6ba8762df7bf8ea23527eb3
Andrea <andreagalindo.guerra@gmail.com>
2023-05-31 16:20:13 -0400

    Adds env variable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import EnergyForm from "./components/EnergyForm";
 
 function App() {
+  const [appliances, setAppliances] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/appliances`)
+      .then((response) => {
+        setAppliances(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <EnergyForm />
+      <EnergyForm data={appliances} />
     </div>
   );
 }
