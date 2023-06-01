52ac413f3ff268499f294b4817825dc1cec3bdf7
Andrea <andreagalindo.guerra@gmail.com>
2023-06-01 17:37:14 -0400

    Adds functionality to handle adding rows to appliance component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import EnergyForm from "./components/EnergyForm";
+import ApplianceTable from "./components/ApplianceTable";
 
 function App() {
   const [appliances, setAppliances] = useState([]);
+  const [applianceTable, setApplianceTable] = useState([]);
 
   useEffect(() => {
     axios
@@ -18,9 +20,22 @@ function App() {
       });
   }, []);
 
+  const handleAddRow = (newAppliance) => {
+    // Find if the array contains an object by comparing the property value
+    if (applianceTable.some((item) => item.name !== newAppliance.name)) {
+      const rows = [...applianceTable];
+      rows.push(newAppliance);
+      setApplianceTable(rows);
+      // console.log(newAppliance);
+    }
+  };
+
+  const handleDeleteRow = (appliance) => {};
+
   return (
     <div className="App">
-      <EnergyForm data={appliances} />
+      <EnergyForm data={appliances} onAddRow={handleAddRow} />
+      <ApplianceTable rows={applianceTable} />
     </div>
   );
 }
